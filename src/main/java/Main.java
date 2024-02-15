f4686f6e6da6d1c7b21f08458241a757ce07617f
MGD2003D <angrymailru@gmail.com>
2024-02-15 18:13:31 +0300

    feat(pdf-generator): Added support for creating level 1 and level 2 headings and paragraphs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import com.GostHandling.GostSelector;
+import com.generator.pdf.ContentController;
 import com.generator.pdf.PdfGenerator;
 import com.generator.pdf.GostTranslator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -11,29 +13,29 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        System.out.println("Choose document format: ODT, PDF, DOCX");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose document format: ODT, PDF, DOCX");
         String format = scanner.nextLine().toUpperCase();
 
-        if ("PDF".equals(format)) {
-            Map<String, Object> gostDetails = GostSelector.selectGost();
-            if (gostDetails != null) {
-                logger.info("Selected GOST details: {}", gostDetails);
-
-
-                System.out.println("Enter the text for your document:");
-                String customText = scanner.nextLine();
-
-                GostTranslator translator = new GostTranslator(gostDetails);
-
-                PdfGenerator pdfGenerator = new PdfGenerator();
-                pdfGenerator.createPdf("docs/custom_document.pdf", translator.getFontName(), customText);
-                System.out.println("PDF document with custom text has been generated.");
-            } else {
-                System.out.println("PDF generation was cancelled due to GOST selection failure.");
-            }
-        } else {
+        if (!"PDF".equals(format)) {
             System.out.println(format + " format support is TODO.");
+            return;
+        }
+
+        Map<String, Object> gostDetails = GostSelector.selectGost();
+        if (gostDetails == null) {
+            System.out.println("PDF generation was cancelled due to GOST selection failure.");
+            return;
+        }
+        GostTranslator translator = new GostTranslator(gostDetails);
+
+        try {
+            PdfGenerator pdfGenerator = new PdfGenerator("docs/custom_document_type2.pdf");
+            ContentController contentController = new ContentController(pdfGenerator, translator);
+            contentController.start();
+            System.out.println("PDF document has been generated.");
+        } catch (IOException e) {
+            logger.error("Error initializing PDF generation: ", e);
         }
     }
 }
--- src/main/java/com/generator/pdf/PdfGenerator.java
+++ src/main/java/com/generator/pdf/PdfGenerator.java
@@ -4,32 +4,59 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.properties.TextAlignment;
 import com.itextpdf.kernel.font.PdfFont;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 public class PdfGenerator {
-
+    private Document document;
     private static final Logger logger = LoggerFactory.getLogger(PdfGenerator.class);
 
-    public void createPdf(String dest, String fontName, String customText) {
-        try {
-            PdfWriter writer = new PdfWriter(dest);
-            PdfDocument pdf = new PdfDocument(writer);
-            Document document = new Document(pdf);
+    public PdfGenerator(String dest) throws IOException {
+        PdfWriter writer = new PdfWriter(dest);
+        PdfDocument pdf = new PdfDocument(writer);
+        this.document = new Document(pdf);
+    }
 
-            PdfFont font = FontMapper.getFont(fontName);
+    public void addHeading(String content, GostTranslator translator, int level) throws IOException {
+        PdfFont font = FontMapper.getFont(translator.getFontName());
+        float size = translator.getFontSize();
+        TextAlignment alignment = TextAlignment.CENTER;
 
-            Paragraph paragraph = new Paragraph(customText)
-                    .setFont(font)
-                    .setFontSize(12);
-            document.add(paragraph);
+        if (level == 1) {
+            size += 6;
+        } else if (level == 2) {
+            size += 4;
+            alignment = TextAlignment.LEFT;
+        }
 
+        Paragraph paragraph = new Paragraph(content)
+                .setFont(font)
+                .setFontSize(size)
+                .setTextAlignment(alignment);
+
+        document.add(paragraph);
+    }
+
+    public void addParagraph(String content, GostTranslator translator) throws IOException {
+        PdfFont font = FontMapper.getFont(translator.getFontName());
+        Paragraph paragraph = new Paragraph(content)
+                .setFont(font)
+                .setFontSize(translator.getFontSize())
+                .setFirstLineIndent(translator.getParagraphIndent())
+                .setMarginLeft(translator.getMarginLeft())
+                .setMarginRight(translator.getMarginRight())
+                .setTextAlignment(TextAlignment.JUSTIFIED);
+
+        document.add(paragraph);
+    }
+
+    public void closeDocument() {
+        if (document != null) {
             document.close();
-            logger.info("PDF document with custom text has been generated successfully.");
-        } catch (IOException e) {
-            logger.error("Error while generating PDF: {}", e.getMessage());
+            logger.info("PDF document has been closed successfully.");
         }
     }
 }
